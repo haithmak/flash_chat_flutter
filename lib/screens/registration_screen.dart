c3d686abf2d0733e154f6e43d9c41bcbad23ecaa
haithmak <haitham.almaqtari@gmail.com>
2021-06-26 00:38:02 +0300

    connect firebase

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'rounded_button.dart';
+import '../constants.dart';
+import 'chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'RegistrationScreen' ;
@@ -7,94 +13,107 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String ? email , password ;
+ // final  _auth =FirebaseAuth.instance ;
+  bool showSpineer = false;
+
+  @override
+  void initState() {
+    super.initState();
+    //https://stackoverflow.com/questions/63492211/no-firebase-app-default-has-been-created-call-firebase-initializeapp-in
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      setState(() {});
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpineer,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value ;
+                },
+
+                decoration: kTextFelidDecoration.copyWith(
+                  hintText: 'Enter your email',
                   ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true ,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value ;
+                },
+                decoration: kTextFelidDecoration.copyWith(
+                  hintText: 'Enter your password',
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(text: 'Register', color: Colors.blueAccent, onPressed:  () async {
+                setState(() {
+                  showSpineer =true ;
+                });
+
+                try {
+                 final user = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email!,
+                      password: password!,
+                  );
+
+                 if(user!=null){
+                   Navigator.pushNamed(context, ChatScreen.id);
+                 }
+
+                  setState(() {
+                    showSpineer =false ;
+                  });
+
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                  else{
+                    print(e);
+                  }
+                  setState(() {
+                    showSpineer =false ;
+                  });
+                }
+              },),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,19 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/screens/login_screen.dart';
 import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'rounded_button.dart';
+//import 'package:animated_text_kit/animated_text_kit.dart';
 
 
 class WelcomeScreen extends StatefulWidget {
-  static String id = 'WelcomeScreen' ;
+  static String id = 'WelcomeScreen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation? animation, animationColor;
+
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+      // upperBound: 100.0,
+    );
+
+    animation = CurvedAnimation(
+      parent: animationController!,
+      curve: Curves.decelerate,
+    );
+
+    // animationColor = ColorTween(
+    //   begin: Colors.yellow,
+    //   end: Colors.green,
+    // ).animate(animationController!);
+
+    //  animationController!.forward();
+
+    // animation!.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     animationController!.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     animationController!.forward();
+    //   }
+    //   print(status);
+    // });
+    //
+    // animationController!.reverse(from: 1.0);
+    // animationController!.addListener(() {
+    //   setState(() {
+    //   //  print(animationController!.value);
+    //    // print(animation!.value);
+    //   });
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController!.dispose();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white , //animationColor!.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -22,10 +75,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Hero(tag: 'logo',
+                Hero(
+                  tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height:
+                        animation!.value * 100.0, // animationController!.value,
                   ),
                 ),
                 Text(
@@ -40,45 +95,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id) ;
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                   Navigator.pushNamed(context, RegistrationScreen.id) ;
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(text: 'Login', color: Colors.lightBlue, onPressed: (){
+              Navigator.pushNamed(context, LoginScreen.id);
+            },),
+            RoundedButton(text: 'Register', color: Colors.blueAccent, onPressed: (){
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            },),
           ],
         ),
       ),
     );
   }
 }
+
